cc75a2291471bb69df91776f1cb4802c1594799e
Marza12 <yuliamarchel26@gmail.com>
2025-11-23 12:19:22 +0700

    Delete evandzakynugroho directory

--- evandzakynugroho/assets/js/animations.js
+++
@@ -1,6 +0,0 @@
-const obs = new IntersectionObserver((entries) => {
-  entries.forEach(e => {
-    if(e.isIntersecting) e.target.classList.add('in-view');
-  });
-}, {threshold:0.12});
-document.querySelectorAll('.fade-up').forEach(el => obs.observe(el));
